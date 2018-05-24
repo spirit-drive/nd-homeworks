0eff4da029e6e81f00b92b6cd0f865ace53b92dc
Igor Zvyagin <spirit-drive@yandex.ru>
2018-05-25 00:56:37 +0300

    3дз

--- stream/index.js
+++ stream/index.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto');
+const { Transform } = require('stream');
 const fs = require('fs');
 const hash = crypto.createHash('md5');
 
@@ -8,3 +9,20 @@ const inputResult = input.pipe(hash);
 
 inputResult.pipe(output);
 inputResult.pipe(process.stdout);
+
+// Не могу сделать, подскажите. Был бы пример.
+// Методические материалы расстраивают. Минимум комментариев :(
+class transform extends Transform {
+    constructor(options) {
+        super(options);
+    }
+
+    _transform(chunk, encoding, callback) {
+        this.push(chunk);
+        callback();
+    }
+}
+
+let inputTransform = new transform();
+
+inputTransform._transform(hash).pipe(output);
