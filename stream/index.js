ffcbf3987c6c618e0c2aff02b97a5ab6309e3481
Igor Zvyagin <spirit-drive@yandex.ru>
2018-05-25 00:17:23 +0300

    3дз

--- stream/index.js
+++ stream/index.js
@@ -4,9 +4,7 @@ const hash = crypto.createHash('md5');
 
 const input = fs.createReadStream('input.txt');
 const output = fs.createWriteStream('output.txt');
-
-const input2 = fs.createReadStream('output.txt');
-
 const inputResult = input.pipe(hash);
 
-inputResult.pipe(output).pipe(input2).pipe(process.stdout);
+inputResult.pipe(output);
+inputResult.pipe(process.stdout);
